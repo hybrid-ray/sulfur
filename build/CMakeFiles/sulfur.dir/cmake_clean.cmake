25f4292222f980482aa8de5ac88f7717fe382b59
mainuser <mainuser@debian1.com>
2021-08-09 12:48:54 +0200

    First test build

---
+++ build/CMakeFiles/sulfur.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sulfur.dir/src/main.cpp.o"
+  "CMakeFiles/sulfur.dir/src/main.cpp.o.d"
+  "sulfur"
+  "sulfur.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sulfur.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/sulfur.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for sulfur.
+# This may be replaced when dependencies are built.
