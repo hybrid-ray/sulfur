7cde9162b72e671f08524afe118d9a4cbb2fdcc1
mainuser <mainuser@debian1.com>
2021-08-09 15:18:11 +0200

    version cmd argument

--- src/cfgs.hpp
+++ src/cfgs.hpp
@@ -1,6 +1,7 @@
 #ifndef CFGS_HPP
 #define CFGS_HPP
 
-
+constexpr int SULFUR_VERSION_MAJOR = 0;
+constexpr int SULFUR_VERSION_MINOR = 0;
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
+#include <string>
+#include "cfgs.hpp"
 
-int main() {
+int main(int argc, char **argv) {
+	bool help = argc == 1;
+	for (int i = 1; i < argc; i++) {
+		std::string arg = std::string(argv[i]);
+		if (arg[0] == '-') {
+			switch (arg[1]) {
+			case 'v':
+				std::cout << "Sulfur compiler " << SULFUR_VERSION_MAJOR << "." << SULFUR_VERSION_MINOR << std::endl;
+				break;
+			case 'h':
+			case '?':
+				help = true;
+				break;
+			}
+		}
+	}
 	std::cout << "Hi!" << std::endl;
 	return 0;
 }
